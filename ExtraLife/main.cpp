25072e126349fbf945dfe486c829996cc0efd76d
Malpp <cpt.ortan@gmail.com>
2019-11-02 11:15:07 -0400

    Escape to close

--- ExtraLife/main.cpp
+++ ExtraLife/main.cpp
@@ -12,6 +12,9 @@ int main()
 		sf::Event event{};
 		while (window.pollEvent(event))
 		{
+			if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::Escape)
+				window.close();
+			
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
